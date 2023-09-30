a7bb71bbb5e2ae49725ac39eccdd4db6ae5fab37
chrissilb <christian.silberbauer@gwasch.de>
2023-09-30 20:09:37 +0200

    update

--- src/main/java/de/gwasch/code/demoproductionsystem/Generate.java
+++ src/main/java/de/gwasch/code/demoproductionsystem/Generate.java
@@ -5,7 +5,7 @@ import de.gwasch.code.escframework.components.utils.CodeGenerator;
 public class Generate {
 	public static void main(String[] args) throws Exception {		
 		
-		CodeGenerator generator = new CodeGenerator("src/main/java", Generate.class.getPackageName(), "src/main/resources");
+		CodeGenerator generator = new CodeGenerator("src/main/java", Generate.class.getPackageName(), "target/generated-sources");
 		generator.generateInterfaces();
 	}
 }
